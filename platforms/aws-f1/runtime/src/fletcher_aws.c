5c8bf27d04dfd2e66833e125598d4525f5123ff6
L. Wijtemans <l.wijtemans@student.tudelft.nl>
2019-04-10 20:49:35 +0200

    aws runtime: parallelize D2H copy

--- platforms/aws-f1/runtime/src/fletcher_aws.c
+++ platforms/aws-f1/runtime/src/fletcher_aws.c
@@ -270,6 +270,9 @@ fstatus_t platformCopyDeviceToHost(da_t device_source, uint8_t *host_destination
   }
   size_t qbytes = (size_t)(size / queues);
 
+  bool error = false;
+
+  #pragma omp parallel for numthreads(FLETCHER_AWS_NUM_QUEUES), default(shared), schedule(static, 1)
   for (int q = 0; q < queues; q++) {
     ssize_t rc = 0;
     // Determine number of bytes for the whole transfer
@@ -294,19 +297,21 @@ fstatus_t platformCopyDeviceToHost(da_t device_source, uint8_t *host_destination
         int errsv = errno;
         fprintf(stderr, "[FLETCHER_AWS] Copy device to host failed. Queue: %d. Error: %s\n", q, strerror(errsv));
         aws_state.error = 1;
-        return FLETCHER_STATUS_ERROR;
+        error = true;
       }
       read[q] += rc;
     }
   }
+
   for (int q = 0; q < queues; q++) {
     total += read[q];
-
-    // Synchronize the files
-    fsync(aws_state.xdma_rd_fd[q]);
   }
 
-  return FLETCHER_STATUS_OK;
+  if (error) {
+    return FLETCHER_STATUS_ERROR;
+  } else {
+    return FLETCHER_STATUS_OK;
+  }
 }
 
 fstatus_t platformTerminate(void *arg) {
